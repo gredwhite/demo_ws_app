761f401ee01c45b5071a1f38dd1490ce928d9360
ntkachev <Nikolay.Tkachev@dataart.com>
2018-04-27 13:29:26 +0300

    configured websocket security

--- src/main/java/hello/WebSocketAuthorizationSecurityConfig.java
+++ src/main/java/hello/WebSocketAuthorizationSecurityConfig.java
@@ -10,6 +10,9 @@ public class WebSocketAuthorizationSecurityConfig extends AbstractSecurityWebSoc
     protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
         // You can customize your authorization mapping here.
         messages.anyMessage().authenticated();
+        messages.simpDestMatchers("/app/hello").authenticated()//.hasRole("ADMIN")
+                .simpSubscribeDestMatchers("/user/queue/**").hasRole("ADMIN")
+                .simpSubscribeDestMatchers("/topic/greetings").authenticated();
     }
 
     // TODO: For test purpose (and simplicity) i disabled CSRF, but you should re-enable this and provide a CRSF endpoint.
